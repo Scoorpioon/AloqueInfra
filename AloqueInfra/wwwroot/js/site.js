3602876402561a4b013dc9108f22aa5e3806bedf
Scoorpioon <gabrielalexandre1337@gmail.com>
2024-02-20 16:35:16 -0300

    Atualização automática de valor diário

--- AloqueInfra/wwwroot/js/site.js
+++ AloqueInfra/wwwroot/js/site.js
@@ -1,10 +1,36 @@
 ﻿const valorAlocacao = document.getElementById("ValorAlocacao");
 const valorFechado = document.getElementById("ValorFechado");
+const valorAlocacaoAutomatico = document.getElementById("ValorAlocacaoAutomatico");
+const labelPreencher = document.getElementById("campoPreencher");
+const labelAutomatico = document.getElementById("campoAutomatico");
+const selecaoFuncionario = document.getElementById("Funcionario");
 
-console.log(valorFechado);
+valorAlocacaoAutomatico.style.visibility = "hidden", labelAutomatico.style.visibility = "hidden";
+valorAlocacaoAutomatico.disabled = true;
 
 valorFechado.addEventListener('change', (acao) => {
-    console.log(acao);
+    let opcaoSelecionada = valorFechado.value;
+    console.log(opcaoSelecionada);
 
-    opcaoSelecionada == true ? valorAlocacao.disabled = true : valorAlocacao.disabled = false;
-};
+    if (valorAlocacao.disabled) {
+        valorAlocacao.style.visibility = "visible", labelPreencher.style.visibility = "visible";
+        valorAlocacao.disabled = false;
+
+        valorAlocacaoAutomatico.style.visibility = "hidden", labelAutomatico.style.visibility = "hidden";
+        valorAlocacaoAutomatico.disabled = true;
+    } else {
+        valorAlocacao.style.visibility = "hidden", labelPreencher.style.visibility = "hidden";
+        valorAlocacao.disabled = true;
+
+        valorAlocacaoAutomatico.style.visibility = "visible", labelAutomatico.style.visibility = "visible";
+        valorAlocacaoAutomatico.disabled = false;
+    }
+});
+
+selecaoFuncionario.addEventListener('change', () => {
+    let valorSelecionado = selecaoFuncionario.selectedIndex;
+    let opcaoSelecionada = selecaoFuncionario.options[valorSelecionado];
+    let data = opcaoSelecionada.getAttribute("data-valor");
+
+    valorAlocacaoAutomatico.value = opcaoSelecionada.dataset.valor;
+});
